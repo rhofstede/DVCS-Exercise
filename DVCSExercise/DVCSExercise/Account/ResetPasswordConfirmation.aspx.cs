32323a7c9b2b598260808500638f13dde4b004b2
rhofstede <31122066+rhofstede@users.noreply.github.com>
2019-09-13 11:44:47 -0600

    Created webapp DVCS_Website

---
+++ DVCSExercise/DVCSExercise/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DVCSExercise.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DVCSExercise/DVCSExercise/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DVCSExercise.Global" Language="C#" %>
